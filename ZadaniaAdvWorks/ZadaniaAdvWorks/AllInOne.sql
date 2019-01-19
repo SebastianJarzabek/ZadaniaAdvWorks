4a478a608b7b6696002f75e28ce15c2bcdd58398
Sebastian Jarząbek <sebastian.jarzabek@outlook.com>
2019-01-19 17:14:32 +0100

    fast transition and random tasks

---
+++ ZadaniaAdvWorks/ZadaniaAdvWorks/AllInOne.sql
@@ -0,0 +1,12 @@
+use AdventureWorks
+go
+
+select 
+month(ss.OrderDate) as 'month'
+,ss.SalesPersonID
+,ss.TerritoryID
+from Sales.SalesOrderHeader as ss
+where ss.OrderDate between '2012-01-01' and ' 2012-03-31'
+and ss.SalesPersonID is not null and ss.TerritoryID is not null
+group by rollup (ss.OrderDate,ss.SalesPersonID,ss.TerritoryID)
+
